7657678cb1c15495c8ed4439e8cc9a96702d6970
Bruno <mct.bruno@gmail.com>
2023-03-13 11:59:25 +0100

    Add TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './aasets/**/*.js',
+    './template/**/*.html.twig'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
