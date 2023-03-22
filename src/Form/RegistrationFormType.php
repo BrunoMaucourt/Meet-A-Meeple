4720a60f8626716e162f26a68ed50e471159e33f
Bruno <mct.bruno@gmail.com>
2023-03-22 13:50:50 +0100

    MEET-29 Registration system

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -13,14 +15,19 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\File;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('last_name')
-            ->add('first_name')
+            ->add('last_name', TextType::class, [
+                'required' => true,
+            ])
+            ->add('first_name', TextType::class, [
+                'required' => true,
+            ])
             ->add('gender_id', ChoiceType::class, [
                 'choices'  => [
                     'Non précisé' => 0,
@@ -31,7 +38,13 @@ class RegistrationFormType extends AbstractType
             ->add('pseudo')
             ->add('birthdate')
             ->add('city')
-            ->add('description')
+            ->add('city_GPS_lat', TextareaType::class,[
+            'label' => false]
+            )
+            ->add('city_GPS_long', TextareaType::class,[
+                'label' => false]
+                )
+            ->add('description', TextareaType::class)
             ->add('favorite_game')
             ->add('trait1_id', ChoiceType::class, [
                 'choices'  => [
@@ -65,7 +78,20 @@ class RegistrationFormType extends AbstractType
                     'Option 4' => 3,
                 ]
             ])
-            ->add('picture_profil', FileType::class)
+            ->add('picture_profil', FileType::class, [
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'maxSizeMessage' => 'La taille de la photo de profil est supérieure à 1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp',
+                        ],
+                        'mimeTypesMessage' => 'Le format de la photo de profil ne correspond pas à JPEG, PNG or WEBP format',
+                    ])
+                ]
+            ])
             ->add('email')
             ->add('RGPDconsent', CheckboxType::class, [
                 'mapped' => false,
