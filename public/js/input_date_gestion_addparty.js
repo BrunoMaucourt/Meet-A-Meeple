f94da2eeac3e7d4e19907759b232beed2cb3536b
Bruno <mct.bruno@gmail.com>
2023-04-01 08:15:37 +0200

    pre-fill player number needed in add party form

--- public/js/input_date_gestion_addparty.js
+++ public/js/input_date_gestion_addparty.js
@@ -1,18 +1,20 @@
 const date_first_input = document.getElementById("date_time_party");
 const date_last_input = document.getElementById("last_date_party");
 const date_current = Date();
-console.log(date_current);
-date_first_input.addEventListener("onchange", function() {console.log("Ferdi0")});
+
 if(date_first_input === date_first_input) {
 document.querySelector("#date_time_party").onchange = function () {
-    console.log("Ferdi2");
     let input = document.getElementById("last_date_party");
     input.setAttribute("max", this.value);
-    input.setAttribute("min", date_current);
 }};
 
+/**
+ * Pre-fill field number needed when number total is changed
+ */
+const player_number_needed = document.getElementById("player_number_needed");
+const player_number_total = document.getElementById("player_number_total");
 
-
-
-
-
+player_number_total.addEventListener("change", function(){
+    player_number_needed.value = player_number_total.value -1;
+    player_number_needed.setAttribute("max", player_number_total.value);
+})
